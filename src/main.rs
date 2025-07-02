7761d0c0de948b02eac42c297ef8e79f41d9a39d
Devansh Tyagi <devanshtyagi0607@gmail.com>
2025-07-02 17:19:19 +0530

    bootable hello world!

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@
 
 use core::panic::PanicInfo;
 
-// entry point of the program [doesnt return anything]
+static HELLO: &[u8] = b"Hello World!";
+
+// entry point of the program
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
-
 // just loop infinitely for now if panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
