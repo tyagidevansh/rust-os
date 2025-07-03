f5979e7a8317b20928df7a7b0ab473d13eaebcc8
Devansh Tyagi <devanshtyagi0607@gmail.com>
2025-07-03 23:07:04 +0530

    epic vga

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,19 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
 
 // entry point of the program
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("hello world!!!!!!!!!");
+    panic!("some panic message");
     loop {}
 }
 // just loop infinitely for now if panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
