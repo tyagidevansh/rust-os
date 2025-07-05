3f036d8d2da9479467fda8be94a00520df0cbcc3
Devansh Tyagi <devanshtyagi0607@gmail.com>
2025-07-05 18:51:51 +0530

    comprehensive testing framework

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 #![no_std] // dont link the std lib
 #![no_main] // disable rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-
+use rust_os::println;
+use rust_os::serial_println;
 
 // entry point of the program
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("hello world!!!!!!!!!");
-    panic!("some panic message");
+    serial_println!("main main main");   
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
-// just loop infinitely for now if panic
+
+#[cfg(not(test))] // conditional compilation
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
